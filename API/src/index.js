703505ea79835cb6ae71d05e84c809374f1d6734
JoaoMaFase <nascc.joao@gmail.com>
2023-08-24 15:17:15 -0300

    ok

--- API/src/index.js
+++ API/src/index.js
@@ -1,11 +1,14 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import agendaController from './controller/agendaController.js';
 
 let servidor = express();
 servidor.use(cors());
 servidor.use(express.json());
 servidor.use();
+
+servidor.use(agendaController);
 
 servidor.listen(
     process.env.PORT,
